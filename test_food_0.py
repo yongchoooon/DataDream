7627bfa3bd9c9a64479f19fd82d933bd562edde0
yongchoooon <sod7050@gmail.com>
2025-05-15 05:43:04 +0000

    code

---
+++ test_food_0.py
@@ -0,0 +1,12 @@
+import os
+from glob import glob
+
+paths_38 = sorted(glob('aa-datadream/food-*-1/aug-*-0.png'))
+paths_39 = sorted(glob('aa-datadream/food-*-1/aug-*-1.png'))
+print(paths_38)
+
+for path_38, path_39 in zip(paths_38, paths_39):
+    print(path_38)
+    print(path_39)
+    # path_39를 path_38로 복사
+    os.system(f'cp {path_39} {path_38}')
